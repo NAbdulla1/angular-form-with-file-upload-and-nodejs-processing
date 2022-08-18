4ab4b5ed83bbd9f74e660fb937dba693786c7758
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2022-08-18 14:11:56 +0600

    add payload type

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Payload } from './payload';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +10,22 @@ import { Observable, switchMap } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   @ViewChild('fileForm') fileForm!: ElementRef;
-  images$?: Observable<{ name: string, url: string }[]>;
+  images$?: Observable<Payload[]>;
   urlBase = 'http://localhost:5000';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.images$ = this.http.get<{ name: string, url: string }[]>(this.urlBase);
+    this.images$ = this.http.get<Payload[]>(this.urlBase);
   }
 
   submitForm() {
     const formData = new FormData(this.fileForm.nativeElement);
 
-    this.http.post('http://localhost:5000/', formData).subscribe({
-      next: (data) => {
-        console.log(data);
-        this.images$ = this.http.get<{ name: string, url: string }[]>(this.urlBase);
+    this.http.post<Payload>('http://localhost:5000/', formData).subscribe({
+      next: (data: Payload) => {
+        console.log('response: ', data);
+        this.images$ = this.http.get<Payload[]>(this.urlBase);
       },
       error: (err) => console.log(err),
       complete: () => console.log('complete'),
